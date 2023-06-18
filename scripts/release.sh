08a0231aa803c0dc5c169d6fc891e25369f69703
gn5r <shangyuan.tuolang@gmail.com>
2023-06-18 17:01:47 +0900

    chore: remove `npm whoami` command

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-npm config set //npm.pkg.github.com/:_authToken "${NODE_AUTH_TOKEN:?}"
-npm whoami
+npm config set //npm.pkg.github.com/:_authToken ${NODE_AUTH_TOKEN:?}
 npm publish --tag "$(node ./scripts/parse-dist-tag.cjs)"
